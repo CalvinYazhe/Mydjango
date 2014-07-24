d49cc76bc78b0aa860a0911b58bdf7e63f2192c9
CalvinYazhe <wyz147159@163.com>
2014-07-24 18:59:53 +0800

    Update the blog models and templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from django.db import models
 from django.contrib import admin
 
@@ -7,29 +8,51 @@ class BlogsPost(models.Model):
     body = models.TextField()
     timestamp = models.DateTimeField()
 
-class RealServer(models.Model):
-    ServerName = models.CharField(max_length = 150)
-    WlanIp = models.IPAddressField()
-    LanIp = models.IPAddressField()
-    ManagerIp = models.IPAddressField()
-    Usefor = models.CharField(max_length = 150)
-    Notification = models.BooleanField()
+class Person(models.Model):
+    name = models.CharField('作者姓名', max_length=10)
+    age = models.IntegerField('作者年龄')
 
-class LvsServer(models.Model):
-    ServerId = models.ForeignKey(RealServer, unique=True)
-    ProxyGroupId = models.IntegerField(max_length = 100)
-    DomainNameId = models.IntegerField(max_length = 100)
-    Status = models.CharField(max_length = 150)
+class Book(models.Model):
+    person =models.ManyToManyField(Person, related_name='person_book')
+    title =models.CharField('书籍名称', max_length=10)
+    pubtime =models.DateField('出版时间')
 
-class ProxyServer(models.Model):
-    ProxyServerId = models.IntegerField(max_length = 100)
-    ProxyGroupId = models.IntegerField(max_length = 100)
-    ServerId = models.ForeignKey(RealServer, unique=True)
+#class RealServer(models.Model):
+#    ServerName = models.CharField(max_length = 150)
+#    WlanIp = models.IPAddressField()
+#    LanIp = models.IPAddressField()
+#    ManagerIp = models.IPAddressField()
+#    Usefor = models.CharField(max_length = 150)
+#    Notification = models.BooleanField()
+#
+#class LvsServer(models.Model):
+#    ServerId = models.ForeignKey(RealServer, unique=True)
+#    ProxyGroupId = models.IntegerField(max_length = 100)
+#    DomainNameId = models.IntegerField(max_length = 100)
+#    Status = models.CharField(max_length = 150)
+#
+#class LvsGroup(models.Model):
+#    LvsGroupId = models.IntegerField(max_length = 100)
+#    LvsGroupName = models.CharField(max_length = 150)
+#    Servers = models.ManyToMany()
+#
+#class ProxyServer(models.Model):
+#    ProxyServerId = models.IntegerField(max_length = 100)
+#    ProxyGroupId = models.IntegerField(max_length = 100)
+#    ServerId = models.ForeignKey(RealServer, unique=True)
+#
+#class ProxyGroup(models.Model):
+#    GroupId = models.IntegerField(max_length = 100)
+#    ProxyServerId = models.ForeignKey(ProxyServer, unique=True)
+#
+#class DomainList(models.Model):
+#    DomainName = models.CharField(max_length = 150)
+#    ProxyGroupId = models.ForeignKey(ProxyGroup, unique=True)
+#    LvsGroupId = models.ForeignKey(LvsGroup, unique=True)
+#    AppServerGroupId = models.ForeignKey(AppServerGroup, unique=True)
 
-class ProxyGroup(models.Model):
-    GroupId = models.IntegerField(max_length = 100)
-    ProxyServerId = models.ForeignKey(ProxyServer, unique=True)
-
-admin.site.register(BlogsPost)
-admin.site.register(RealServer)
-admin.site.register(ProxyServer)
+#admin.site.register(BlogsPost)
+#admin.site.register(RealServer)
+#admin.site.register(ProxyServer)
+admin.site.register(Person)
+admin.site.register(Book)
